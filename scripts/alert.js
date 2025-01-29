a00a7fe9701564f109a0a9abbb08043c84195d7b
nathanjtaylor <144626570+nathanjtaylor@users.noreply.github.com>
2025-01-29 14:56:51 +0000

    alert finish

--- scripts/alert.js
+++ scripts/alert.js
@@ -1,6 +1,6 @@
-let alerted = parseFloat(localStorage.getItem("alerted")) || false;
-console.log(alerted);
-if (alerted === false) {
-    alert("hello");
+let alerted = localStorage.getItem("alerted") === "true";
+
+if (!alerted) {
+    alert("My camera was accidentially damaged before taking off so some of the images may have defects.");
     localStorage.setItem("alerted", true);
 }
