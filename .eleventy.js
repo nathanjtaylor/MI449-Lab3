038e8fcbdc3f23e0452e6cf3f1b919308850cf7c
nathanjtaylor <144626570+nathanjtaylor@users.noreply.github.com>
2025-01-29 16:43:48 +0000

    shortcode and sources

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("scripts");
-};
+
+  eleventyConfig.addShortcode(
+    "blogs",
+    (title, href, src, alt) => {
+      return `<a href="${href}"><img alt="${alt}" src="${src}"/><div>${title}</div></a>`;
+    }
+  );
+}
